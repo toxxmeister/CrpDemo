cec9c1824afd47cafa73ae3c3e4e916c18408038
Boris Generalic <boris.generalic@gmail.com>
2017-05-25 16:08:18 +0200

    Started service binding.

--- app/src/main/java/de/troido/crpdemo/MainActivity.kt
+++ app/src/main/java/de/troido/crpdemo/MainActivity.kt
@@ -1,11 +1,17 @@
 package de.troido.crpdemo
 
 import android.Manifest
+import android.content.ComponentName
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.IBinder
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import de.troido.crowdroutesdk.service.CrpListener
+import de.troido.crowdroutesdk.service.CrpMessage
 import de.troido.crowdroutesdk.service.CrpService
+import de.troido.crowdroutesdk.service.CrpService.CrpServiceBinder
 import de.troido.crpdemo.util.check
 import de.troido.crpdemo.util.longToast
 import de.troido.crpdemo.util.startService
@@ -18,6 +24,35 @@ private val PERMISSIONS = arrayOf(
 )
 
 class MainActivity : AppCompatActivity() {
+
+    val serviceConnection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder: CrpServiceBinder = service as CrpServiceBinder
+
+            val listener = object: CrpListener {
+                override val id: Short
+                    get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+                override fun onAckDelivered() {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onAckReceived() {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onMessage(msg: CrpMessage) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
